58ab6cc1b81363766d7386d95107f3031b82e73a
Josh Rolstad <jorolsta@microsoft.com>
2019-12-31 15:05:25 -0800

    Adding tests to verify functionality

---
+++ azuredevops-resourceanalyzer.tests/Managers/ProjectSummaryManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using azuredevopsresourceanalyzer.core.Managers;
+using azuredevopsresourceanalyzer.core.Services;
+using Xunit;
+
+namespace azuredevops_resourceanalyzer.tests.Managers
+{
+    public class ProjectSummaryManagerTests
+    {
+        [Fact]
+        public void GetSummary_ValidInputs_CreatesSummary()
+        {
+            // Given
+            var organization = "microsoftit";
+            var project = "oneitvso";
+            var filter = "sr-im";
+            DateTime? startDate = null;
+            var manager = GetInstance();
+
+            // When
+            var result = manager.GetSummary(organization, project, filter);
+
+            // Then
+            Assert.NotNull(result);
+            Assert.Equal(organization,result.Organization);
+            Assert.Equal(project,result.Project);
+            Assert.Equal(filter,result.RepositoryFilter);
+            Assert.Equal(startDate,result.StartDate);
+
+            Assert.NotEmpty(result.Components);
+        }
+
+        private ProjectSummaryManager GetInstance()
+        {
+            return new ProjectSummaryManager(new AzureDevopsService());
+        }
+    }
+}
---
+++ azuredevops-resourceanalyzer.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace azuredevops_resourceanalyzer.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
