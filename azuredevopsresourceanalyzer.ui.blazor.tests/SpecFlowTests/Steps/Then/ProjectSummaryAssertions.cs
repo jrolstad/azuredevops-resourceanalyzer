0e83c50a604638464723a1f6899bcf568bdf33e8
Josh Rolstad <jrolstad@gmail.com>
2020-01-07 18:10:48 -0800

    Further refactoring of asserts for shared steps

--- azuredevopsresourceanalyzer.ui.blazor.tests/SpecFlowTests/Steps/Then/ProjectSummaryAssertions.cs
+++ azuredevopsresourceanalyzer.ui.blazor.tests/SpecFlowTests/Steps/Then/ProjectSummaryAssertions.cs
@@ -27,28 +27,6 @@ namespace azuredevopsresourceanalyzer.ui.blazor.tests.SpecFlowTests.Steps.Then
             Assert.Equal(count,componentsOnView.Count);
         }
 
-        [Then(@"the selected project is '(.*)'")]
-        public void ThenTheSelectedProjectIs(string project)
-        {
-            Assert.Equal(project,_context.ProjectSummary().Project);
-        }
-
-        [Then(@"the list of projects shown are")]
-        public void ThenTheListOfProjectsShownAre(Table table)
-        {
-            var expected = table.Rows
-                .Select(r => r[0].Trim())
-                .ToList();
-
-            Assert.Equal(expected,_context.ProjectSummary().Projects);
-        }
-
-        [Then(@"no errors are shown")]
-        public void NoErrorsAreShown()
-        {
-            Assert.Null(_context.ProjectSummary().Error);
-        }
-
         [Then(@"the project summary results contain repositories")]
         public void ThenTheProjectSummaryResultsContainRepositories(Table table)
         {
---
+++ azuredevopsresourceanalyzer.ui.blazor.tests/SpecFlowTests/Steps/Then/SummaryAssertions.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using azuredevopsresourceanalyzer.ui.blazor.tests.SpecFlowTests.Steps.Extensions;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace azuredevopsresourceanalyzer.ui.blazor.tests.SpecFlowTests.Steps.Then
+{
+    [Binding]
+    public class SummaryAssertions
+    {
+        private readonly ScenarioContext _context;
+
+        public SummaryAssertions(ScenarioContext context)
+        {
+            _context = context;
+        }
+
+        [Then(@"the selected project is '(.*)'")]
+        public void ThenTheSelectedProjectIs(string project)
+        {
+            switch (_context.ViewType())
+            {
+                case ViewType.ProjectSummary:
+                {
+                    Assert.Equal(project, _context.ProjectSummary().Project);
+                        break;
+                }
+                case ViewType.WorkSummary:
+                {
+                    Assert.Equal(project, _context.WorkSummary().Project);
+                        break;
+                }
+            }
+            
+        }
+
+        [Then(@"the list of projects shown are")]
+        public void ThenTheListOfProjectsShownAre(Table table)
+        {
+            var expected = table.Rows
+                .Select(r => r[0].Trim())
+                .ToList();
+
+            switch (_context.ViewType())
+            {
+                case ViewType.ProjectSummary:
+                {
+                    Assert.Equal(expected, _context.ProjectSummary().Projects);
+                    break;
+                }
+                case ViewType.WorkSummary:
+                {
+                    Assert.Equal(expected, _context.WorkSummary().Projects);
+                    break;
+                }
+            }
+        }
+
+        [Then(@"no errors are shown")]
+        public void NoErrorsAreShown()
+        {
+            switch (_context.ViewType())
+            {
+                case ViewType.ProjectSummary:
+                {
+                    Assert.Null(_context.ProjectSummary().Error);
+                    break;
+                }
+                case ViewType.WorkSummary:
+                {
+                    Assert.Null(_context.WorkSummary().Error);
+                    break;
+                }
+            }
+        }
+
+    }
+}
