014531115c18854b225db5aaaed1382935e555e4
Josh Rolstad <jorolsta@microsoft.com>
2020-01-10 09:32:56 -0800

    Stubbing out client with auth

--- azuredevopsresourceanalyzer.ui.blazorclient/Startup.cs
+++ azuredevopsresourceanalyzer.ui.blazorclient/Startup.cs
@@ -1,3 +1,4 @@
+using Des.Blazor.Authorization.Msal;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,11 +8,27 @@ namespace azuredevopsresourceanalyzer.ui.blazorclient
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthorizationCore();
+
+            var myConfig = new MsalConfig
+            {
+                ClientId = "b08edd15-e207-4dd4-86af-44d13723b9b6",
+                Authority = "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47",
+                LoginMode = LoginModes.Redirect
+            };
+            services.AddAzureActiveDirectory(myConfig);
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
             app.AddComponent<App>("app");
         }
+    }
+
+    public class MsalConfig : IMsalConfig
+    {
+        public string ClientId { get; set; }
+        public string Authority { get; set; }
+        public LoginModes LoginMode { get; set; }
     }
 }
