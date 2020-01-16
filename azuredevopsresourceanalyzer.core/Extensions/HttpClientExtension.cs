de1933f0adb581698a0208ab22c5297c6d6cb898
Josh Rolstad <jrolstad@gmail.com>
2020-01-15 21:29:27 -0800

    Building in times to try to get token if it fails

--- azuredevopsresourceanalyzer.core/Extensions/HttpClientExtension.cs
+++ azuredevopsresourceanalyzer.core/Extensions/HttpClientExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -10,9 +11,23 @@ namespace azuredevopsresourceanalyzer.core.Extensions
         public static async Task<T> GetAsJson<T>(this HttpClient client, string url)
         {
             var result = await client.GetAsync(url);
-            var data = await result.Content.ReadAsAsync<T>();
 
-            return data;
+            try
+            {
+                result.EnsureSuccessStatusCode();
+                var data = await result.Content.ReadAsAsync<T>();
+
+                return data;
+            }
+            catch (Exception e)
+            {
+                var requestAuthorization = result.RequestMessage?.Headers?.Authorization?.Parameter;
+                
+                var message = $"Unable to parse result for {url}\r\nStatus is {result.StatusCode}\r\nAccess Token is{requestAuthorization}";
+                
+                throw new ApplicationException(message,e);
+            }
+           
         }
 
         
--- azuredevopsresourceanalyzer.ui.blazorclient/Application/Services/AzureAdUserTokenService.cs
+++ azuredevopsresourceanalyzer.ui.blazorclient/Application/Services/AzureAdUserTokenService.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using azuredevopsresourceanalyzer.core.Services;
 using Des.Blazor.Authorization.Msal;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace azuredevopsresourceanalyzer.ui.blazorclient.Application
 {
@@ -15,9 +16,18 @@ namespace azuredevopsresourceanalyzer.ui.blazorclient.Application
         }
         public async Task<string> GetBearerToken(string resource)
         {
-            var token = await _authenticationManager.GetAccessTokenAsync("");
-            
-            return token?.AccessToken;
+            for (var step = 0; step < 3; step++)
+            {
+                var token = await _authenticationManager.GetAccessTokenAsync("");
+
+                if (!string.IsNullOrWhiteSpace(token?.AccessToken))
+                {
+                    return token?.AccessToken;
+                }
+            }
+
+            return null;
+
         }
     }
 }
